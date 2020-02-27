0fe87d161dbbb79b37a132d3e9725dad8df40352
Dana Hooshmand <11295476+hooshd@users.noreply.github.com>
2020-02-27 15:01:26 -0500

    removed the dana components.

--- src/templates/index.js
+++ src/templates/index.js
@@ -21,16 +21,6 @@ const Index = ({ data, location, pageContext }) => {
             <MetaData location={location} />
             <Layout isHome={true}>
                 <div className="container">
-                    <section class="aboutgrid"> /*dana*/
-                        <a class="post-card">
-                            <h2 class="post-card-title">About Dana Hooshmand</h2>
-                            <p class="post-card-excerpt">I write about startup operations, technology, coding and motorcycles. I create guides on doing difficult things with minimal resources. I also write about exploring foreign cultures by studying language, sport and art at my other site, Discover Discomfort.</p>
-                        </a>
-                        <a class="post-card">
-                            <div class="post-card-image" style="background-image: url(&quot;https://hooshmand.net/content/images/size/w1000/2019/12/Dana---motorcycle-1.jpg&quot;);"></div>
-                        </a>
-                    </section>
-                    <hr>
                     <section className="post-feed">
                         {posts.map(({ node }) => (
                             // The tag below includes the markup for each post - components/common/PostCard.js
