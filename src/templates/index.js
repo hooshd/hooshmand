c0d54a42ee3fc3d088041f7a2ba13ae3f596a947
Dana Hooshmand <11295476+hooshd@users.noreply.github.com>
2020-02-27 14:04:15 -0500

    added in about section

--- src/templates/index.js
+++ src/templates/index.js
@@ -21,6 +21,33 @@ const Index = ({ data, location, pageContext }) => {
             <MetaData location={location} />
             <Layout isHome={true}>
                 <div className="container">
+                    <!-- Dana -->
+                        <style>
+                            .aboutgrid {
+                                display: grid;
+                                justify-content: space-between;
+                                    grid-gap: 30px;
+                                    grid-template-columns: 1fr 1fr;
+                                }
+
+                                @media (max-width:680px) {
+                                    .aboutgrid {
+                                        grid-template-columns: 1fr
+                                    }
+                                }
+
+                            </style>
+                            <section class="aboutgrid">
+                                <a class="post-card">
+                                    <h2 class="post-card-title">About Dana Hooshmand</h2>
+                                    <p class="post-card-excerpt">I write about startup operations, technology, coding and motorcycles. I create guides on doing difficult things with minimal resources. I also write about exploring foreign cultures by studying language, sport and art at my other site, Discover Discomfort.</p>
+                                </a>
+                                <a class="post-card">
+                                    <div class="post-card-image" style="background-image: url(&quot;https://hooshmand.net/content/images/size/w1000/2019/12/Dana---motorcycle-1.jpg&quot;);"></div>
+                                </a>
+                            </section>
+                            <hr>
+                            <!-- Dana -->
                     <section className="post-feed">
                         {posts.map(({ node }) => (
                             // The tag below includes the markup for each post - components/common/PostCard.js
